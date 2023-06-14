04abfbf2f1bb9591bbaa4d09fb1ae41bf48e4ef9
Gabriel Bessi <tecbessi@gmail.com>
2023-06-13 22:06:16 -0300

    feat<users-configs>: Criação do middleware responsável pela verificação do token passado

---
+++ middleware/check-token.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const checkTokenMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: "Acesso negado" });
+  }
+
+  const tokenAuth = token.split(" ")[1];
+
+  jwt.verify(tokenAuth, process.env.KEYSECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({ message: error.message });
+    }
+
+    req.user = {
+      id: decoded.id,
+    };
+
+    return next();
+  });
+};
+
+module.exports = checkTokenMiddleware;
