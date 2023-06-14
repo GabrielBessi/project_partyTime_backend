a8975b406a7ed10e591c03f3751e2a761e87f53e
Gabriel Bessi <tecbessi@gmail.com>
2023-06-13 22:07:30 -0300

    feat<users-configs>: Middleware responsável pela verificação da existência de usuário e Id

---
+++ middleware/user-exists.js
@@ -0,0 +1,20 @@
+const User = require("../models/user/user.model");
+
+const userExistsMiddleware = async (req, res, next) => {
+  const idParams = req.params.id;
+  const idToken = req.user.id;
+
+  if (idParams !== idToken) {
+    return res.status(400).json({ message: "Erro ao atualizar o usuário" });
+  }
+
+  const userExists = await User.findOne({ _id: idParams });
+
+  if (!userExists) {
+    return res.status(404).json({ message: "Usuário não encontrado" });
+  }
+
+  next();
+};
+
+module.exports = userExistsMiddleware;
